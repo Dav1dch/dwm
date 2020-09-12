78370a0d4f3ea1d3b1013fd7448d4818192131c2
David <jlhuangch@gmail.com>
2020-09-13 00:41:31 +0800

    modify

--- config.h
+++ config.h
@@ -3,19 +3,19 @@
 /* appearance */
 static const unsigned int borderpx = 3; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 6;   /* horiz inner gap between windows */
-static const unsigned int gappiv = 6;   /* vert inner gap between windows */
+static const unsigned int gappih = 4;   /* horiz inner gap between windows */
+static const unsigned int gappiv = 4;   /* vert inner gap between windows */
 static const unsigned int gappoh =
-    6; /* horiz outer gap between windows and screen edge */
+    4; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    6; /* vert outer gap between windows and screen edge */
+    4; /* vert outer gap between windows and screen edge */
 static const int smartgaps =
-    0; /* 1 means no outer gap when there is only one window */
+    0;                              /* 1 means no outer gap when there is only one window */
 static const int showbar = 1;       /* 0 means no bar */
-static const int topbar = 0;        /* 0 means bottom bar */
+static const int topbar = 1;        /* 0 means bottom bar */
 static const Bool viewontag = True; /* Switch view on tag switch */
-static const char *fonts[] = {"SauceCodePro Nerd Font Mono:size=14"};
-static const char dmenufont[] = "SauceCodePro Nerd Font Mono:size=10";
+static const char *fonts[] = {"Consolas NF:size=14"};
+static const char dmenufont[] = "Consolas NF:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
@@ -61,34 +61,40 @@ static const Layout layouts[] = {
     //{ "[]=",      tile },    /* first entry is default */
     //{ "><>",      NULL },    /* no layout function means floating behavior */
     //{ "[M]",      monocle },
-    {"tile", tile}, /* first entry is default */
-    {"NULL", NULL}, /* no layout function means floating behavior */
+    {"Tile", tile},     /* first entry is default */
+    {"Floating", NULL}, /* no layout function means floating behavior */
     {"[M]", monocle},
 };
 
 /* key definitions */
 #define MODKEY Mod1Mask
-#define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
-      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+#define TAGKEYS(KEY, TAG)                                          \
+    {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
+        {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
+        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
+        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                                                             \
-  {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
-  }
+#define SHCMD(cmd)                                           \
+    {                                                        \
+        .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL } \
+    }
 
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1,
+    "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
 static const char *trayercmd[] = {"/home/david/scripts/t-toggle.sh", NULL};
+static const char *upvol[] = {"/home/david/scripts/vol-up.sh", NULL};
+static const char *suspendcmd[] = {"/home/david/scripts/suspend.sh", NULL};
+static const char *incbacklightcmd[] = {"/home/david/scripts/inc-backlight.sh", NULL};
+static const char *decbacklightcmd[] = {"/home/david/scripts/dec-backlight.sh", NULL};
+static const char *downvol[] = {"/home/david/scripts/vol-down.sh", NULL};
+static const char *mutevol[] = {"/home/david/scripts/vol-toggle.sh", NULL};
 static const char *termcmd[] = {"st", NULL};
-static const char *chromecmd[] = {"chromium", NULL};
+static const char *chromecmd[] = {"firefox", NULL};
 static const char *screenshotcmd[] = {"deepin-screenshot", NULL};
 
 static Key keys[] = {
@@ -98,6 +104,12 @@ static Key keys[] = {
     {MODKEY | ShiftMask, XK_t, spawn, {.v = trayercmd}},
     {MODKEY | ShiftMask, XK_a, spawn, {.v = screenshotcmd}},
     {MODKEY | ShiftMask, XK_c, spawn, {.v = chromecmd}},
+    {MODKEY | ShiftMask, XK_s, spawn, {.v = suspendcmd}},
+    {MODKEY, XK_Down, spawn, {.v = downvol}},
+    {MODKEY, XK_Up, spawn, {.v = upvol}},
+    {MODKEY, XK_Left, spawn, {.v = decbacklightcmd}},
+    {MODKEY, XK_Right, spawn, {.v = incbacklightcmd}},
+    {0, XK_Print, spawn, {.v = screenshotcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
